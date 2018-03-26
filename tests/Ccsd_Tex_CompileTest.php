6f924eab6a97945c7c44592b6824b97112238001
Bruno Marmol <Bruno.Marmol@inria.fr>
2018-03-26 17:03:22 +0200

    Fix test Install phpunit Add ressources for tests

--- tests/Ccsd_Tex_CompileTest.php
+++ tests/Ccsd_Tex_CompileTest.php
@@ -43,14 +43,14 @@ class Ccsd_Tex_CompileTest extends PHPUnit_Framework_TestCase
 
     public function provide_checklogs() {
         return [
-            1 => ['check_for_bad_citation',    __DIR__ . '/ressources/logs/foo.log', true],
-            2 => ['check_for_bad_citation',    __DIR__ . '/ressources/logs/good.log', false],
-            3 => ['check_for_bad_natbib',      __DIR__ . '/ressources/logs/Paper.log', true],
-            4 => ['check_for_bad_natbib',      __DIR__ . '/ressources/logs/good.log', false],
-            5 => ['check_for_bad_reference',   __DIR__ . '/ressources/logs/foo.log', true],
-            6 => ['check_for_bad_reference',   __DIR__ . '/ressources/logs/good.log', false],
-            7 => ['check_for_reference_change',__DIR__ . '/ressources/logs/foo.log', true],
-            8 => ['check_for_reference_change',__DIR__ . '/ressources/logs/good.log', false],
+            'a' => ['check_for_bad_citation',    __DIR__ . '/ressources/logs/foo', false],
+            'b' => ['check_for_bad_citation',    __DIR__ . '/ressources/logs/good', false],
+            'c' => ['check_for_bad_natbib',      __DIR__ . '/ressources/logs/Paper', false],  // pas de pb natbib dans paper
+            'd' => ['check_for_bad_natbib',      __DIR__ . '/ressources/logs/good', false],
+            'e' => ['check_for_bad_reference',   __DIR__ . '/ressources/logs/foo', false],
+            'f' => ['check_for_bad_reference',   __DIR__ . '/ressources/logs/good', false],
+            'g' => ['check_for_reference_change',__DIR__ . '/ressources/logs/foo', false],
+            'h' => ['check_for_reference_change',__DIR__ . '/ressources/logs/good', false],
         ];
     }
 }
